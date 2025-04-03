470e2f06358fd374f5e65d367eeffd113350c581
Muhammad <muhammadabdullahgohar572@gmail>
2025-04-04 04:11:45 +0500

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "BUILD START"
-pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+mkdir -p staticfiles_build/static
 echo "BUILD END"
