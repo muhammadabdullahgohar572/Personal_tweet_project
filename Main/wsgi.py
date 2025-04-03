ffff96d3b7700b671c1f79740d1e7320a56f2150
Muhammad <muhammadabdullahgohar572@gmail>
2025-04-04 04:26:41 +0500

    first commit

--- Main/wsgi.py
+++ Main/wsgi.py
@@ -8,7 +8,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Main.settings')
+
 application = get_wsgi_application()
+
+
+app=application
