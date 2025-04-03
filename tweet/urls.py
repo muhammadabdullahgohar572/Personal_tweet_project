bb96a491a458ff09b08443151cf1dd0a69b34019
Muhammad <muhammadabdullahgohar572@gmail>
2025-04-04 02:13:06 +0500

    first commit

--- tweet/urls.py
+++ tweet/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
   
     path('',views.HomePage,name="Home_Page"),
     path('Tweet_Create',views.Tweet_Create,name="Tweet_Create"),
+    path('Deleted_Tweet/<int:emp_id>/',views.DeleteTweet,name="deletetweet"),
+    path('updated_Tweet/<int:emp_id>/',views.Tweet_Update,name="updatedtweet"),
+    path('Tweet_Updated/<int:emp_id>/',views.Updated_Tweet,name="Tweet_Updated"),
     
     
 ]
--- tweet/views.py
+++ tweet/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 
 from   .models import Tweet
 
 # Create your views here.
 
 
+
 def HomePage(request):
-    return render(request,"Navbar/navbar.html")
+   getallData=Tweet.objects.all()
+   return render(request,"tweets/ShowAllTweet.html",{"getallData":getallData})
+
 
 def Tweet_Create(request):
     if request.method == "POST":
@@ -23,3 +26,36 @@ def Tweet_Create(request):
         return redirect("Home_Page")  
     
     return render(request, "tweets/Create_tweet.html")
+
+
+def DeleteTweet(request,emp_id):
+    DeletedTweet=get_object_or_404(Tweet,pk=emp_id)
+    DeletedTweet.delete()
+    return  redirect("Home_Page") 
+
+def Tweet_Update(request,emp_id):
+    UpdatedTweet=get_object_or_404(Tweet,pk=emp_id)
+    return render(request,"tweets/Updated_Tweet.html",{"UpdatedTweet":UpdatedTweet})
+
+
+def Updated_Tweet(request, emp_id):
+    tweet = get_object_or_404(Tweet, pk=emp_id)
+    
+    if request.method == "POST":
+        # Get the form data
+        tweet.text = request.POST.get("Person_text")
+        
+        # Handle image removal
+        remove_image = request.POST.get("remove_image") == "true"
+        if remove_image:
+            tweet.photo.delete()  # Remove the current image
+            tweet.photo = None
+        
+        # Handle new image upload (only if not removing image and new file provided)
+        if 'Person_photo' in request.FILES and not remove_image:
+            tweet.photo = request.FILES['Person_photo']
+        
+        tweet.save()
+        return redirect("Home_Page")
+    
+    return redirect("updatedtweet", emp_id=emp_id)
