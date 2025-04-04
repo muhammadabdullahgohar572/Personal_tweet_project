3b6ce468b92e278112dd38a0ca364a49bf909441
Muhammad <muhammadabdullahgohar572@gmail>
2025-04-05 03:12:03 +0500

    test

--- Main/settings.py
+++ Main/settings.py
@@ -121,7 +121,7 @@ import cloudinary.api
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
